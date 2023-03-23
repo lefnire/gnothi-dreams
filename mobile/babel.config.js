f4ff34ec5ed9696fd2c146015cf665adb3c2746f
Martha Wilding <wilding34@gmail.com>
2023-03-22 17:40:16 -0700

    add paper

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -5,5 +5,10 @@ module.exports = function (api) {
     plugins: [
       require.resolve("expo-router/babel"),
     ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
